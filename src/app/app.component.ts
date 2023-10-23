4ed7d119561eefe72f20c6f40826d4665ac1e476
RamandeepSidhu <ramandeep@pixlerlab.com>
2023-10-23 13:39:18 +0530

    fist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Type } from '@angular/core';
+import { StoryblokService } from './services/storyblok.service';
+import { Components } from './components';
+declare global {
+  interface Window {
+    storyblok: any;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'storyBook';
+  story: any = { content: null, name: '' };
+  components: any = Components;
+
+  constructor(private storyblokService: StoryblokService) {
+    window.storyblok.init();
+    window.storyblok.on(['change', 'published'], function () {
+      location.reload()
+    });
+  }
+
+  ngOnInit() {
+    this.storyblokService.getStory('home', { version: 'draft' })
+      .then(data => this.story = data.story);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { DynamicModule } from 'ng-dynamic-component';
 import { AppComponent } from './app.component';
+import { StoryblokService } from './services/storyblok.service';
+import { StoryblokDirective } from './directives/storyblok.directive';
+import { TeaserComponent } from './components/teaser/teaser.component';
+import { GridComponent } from './components/grid/grid.component';
+import { PageComponent } from './components/page/page.component';
+import { FeatureComponent } from './components/feature/feature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    StoryblokDirective,
+    TeaserComponent,
+    GridComponent,
+    FeatureComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, DynamicModule],
+  exports: [BrowserModule, DynamicModule],
+  providers: [StoryblokService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
